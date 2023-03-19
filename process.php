b60dcdf4b10fa2977c5c967cedf55ea0e815fb84
Tung Pham <tung.42@gmail.com>
2023-03-19 09:52:32 +0700

    Update process.php

--- process.php
+++ process.php
@@ -4,17 +4,13 @@ require_once __DIR__ . '/vendor/autoload.php';
 $client = OpenAI::client('YOUR-API-KEY'); // Get your API Key here: https://platform.openai.com/account/api-keys
 
 $response = $client->chat()->create([
-    'model' => 'gpt-3.5-turbo',
+    'model' => 'gpt-3.5-turbo-0301',
     'messages' => [
         ['role' => 'user', 'content' => $question],
     ],
 ]);
 
 foreach ($response->choices as $result) {
-    $result->index;
-    $result->message->role;
-    $result->message->content;
-    $result->finishReason;
     $answer = $result->message->content;
 }
 
